27e1ef2982c0531c73332a02754b2d443dc59bf5
bocian <bocian@f1a114fe-ac64-4d4c-bfa7-fbcff8671e42>
2011-04-17 09:47:16 +0000

    Uruchomiłem mapera do postgresa, trzeba dorobić ściąganie informacji o wybranej tabeli
    git-svn-id: http://oldfreq.dyndns.org/svn/v3dv/v3dv@16 f1a114fe-ac64-4d4c-bfa7-fbcff8671e42

--- modules/DbConnectors/Postgres.php
+++ modules/DbConnectors/Postgres.php
@@ -9,21 +9,21 @@ Class Postgres extends Db {
 	}
 	
 	function showTables() {
-		/*$res = $this->db->Execute($this->db->metaTablesSQL);
+		$res = $this->db->Execute('SELECT table_name FROM information_schema.tables WHERE table_schema = ?', array('public'));
 		$tables = array();
 		while ($ret = $res->FetchRow()) {
 			$tables[] = array_pop($ret);
 		}
-		return $tables;*/
+		return $tables;
 	}
 	
 	function showDatabases() {
-		/*$res = $this->db->Execute($this->db->metaDatabasesSQL);
+		$res = $this->db->Execute($this->db->metaDatabasesSQL);
 		$databases = array();
 		while ($ret = $res->FetchRow()) {
 			$databases[] = array_pop($ret);
 		}
-		return $databases;*/
+		return $databases;
 	}
 }
 
